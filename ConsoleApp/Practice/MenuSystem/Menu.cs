7764810c2635cd7c419ceb280ad102c7f5fdaba4
Jevgeni Fenko <jefenk@taltech.ee>
2020-09-30 18:19:23 +0300

    unversioned files

---
+++ ConsoleApp/Practice/MenuSystem/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MenuSystem
+{
+    public class Menu
+    {
+        private const string Title = "Main Menu";
+        public void DrawMenu()
+        {
+            Console.WriteLine($"==== {Title} ====");
+        }
+    }
+}
---
+++ ConsoleApp/Practice/Practice01/Program.cs
@@ -0,0 +1,13 @@
+﻿using MenuSystem;
+
+namespace Practice01
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var menu = new Menu();
+            menu.DrawMenu();
+        }
+    }
+}
